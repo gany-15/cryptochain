fbe9f720a17f630095c2751d45f8a7fea92032c9
gany-15 <33432079+gany-15@users.noreply.github.com>
2020-03-15 15:54:50 +0530

    Added static difficulty and nonce

--- blockchain.js
+++ blockchain.js
@@ -14,15 +14,33 @@ class Blockchain {
     this.chain.push(newBlock);
   }
 
+  replaceChain(chain) {
+    if (chain.length <= this.chain.length) {
+      console.error("Incoming chain must be longer");
+      return;
+    }
+    if (!Blockchain.isValidChain(chain)) {
+      console.error("Incoming chain must be valid");
+      return;
+    }
+    this.chain = chain;
+  }
+
   static isValidChain(chain) {
     if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis()))
       return false;
 
     for (let i = 1; i < chain.length; i++) {
-      const { timestamp, lastHash, hash, data } = chain[i];
+      const { timestamp, lastHash, hash, difficulty, nonce, data } = chain[i];
       const actualLastHash = chain[i - 1].hash;
       if (actualLastHash !== lastHash) return false;
-      const validatedHash = cryptohash(timestamp, lastHash, data);
+      const validatedHash = cryptohash(
+        timestamp,
+        lastHash,
+        data,
+        nonce,
+        difficulty
+      );
       if (hash !== validatedHash) return false;
     }
     return true;
