f99c1cb67e9d31e25aa04e188c70fd3e04a50f54
gany-15 <33432079+gany-15@users.noreply.github.com>
2020-03-21 19:33:05 +0530

    added request package, sync chains at start

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const request = require("request");
 const bodyParser = require("body-parser");
 const Blockchain = require("./blockchain");
 const PubSub = require("./pubsub");
@@ -6,6 +7,9 @@ const PubSub = require("./pubsub");
 const app = express();
 const blockchain = new Blockchain();
 const pubsub = new PubSub({ blockchain });
+
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
 setTimeout(() => pubsub.broadcastChain(), 1000);
 
@@ -22,7 +26,19 @@ app.post("/api/mine", (req, res) => {
   res.redirect("/api/blocks");
 });
 
-const DEFAULT_PORT = 3000;
+const syncChains = () => {
+  request(
+    { url: `${ROOT_NODE_ADDRESS}/api/blocks` },
+    (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const rootChain = JSON.parse(body);
+        blockchain.replaceChain(rootChain);
+        console.log("replace chain on sync with ", rootChain);
+      }
+    }
+  );
+};
+
 let PEER_PORT;
 
 if (process.env.GENERATE_PEER_PORT === "true") {
@@ -31,4 +47,7 @@ if (process.env.GENERATE_PEER_PORT === "true") {
 
 const PORT = PEER_PORT || DEFAULT_PORT;
 
-app.listen(PORT, () => console.log(`listening at localhost:${PORT}`));
+app.listen(PORT, () => {
+  console.log(`listening at localhost:${PORT}`);
+  syncChains();
+});
